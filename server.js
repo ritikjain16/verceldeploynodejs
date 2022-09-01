8cf9a95dcb81bf534ea8ec5d3d9e33cb141294df
ritikjain16 <72817734+ritikjain16@users.noreply.github.com>
2022-09-01 22:51:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome To NodeJS Vercel Deploy</h1>");
 });
-
+//bhv
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
