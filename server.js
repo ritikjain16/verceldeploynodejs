b0d2896661a04e1a3ce730c2b8b6d6a870cef8c4
Ritik Jain <ritik9628@gmail.com>
2022-09-01 22:39:22 +0530

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome To NodeJS Vercel Deploy</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
